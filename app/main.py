13d61ba06919f2dd9022878822716566d453edd8
Gabjea <gabjea856@gmail.com>
2026-09-20 16:33:02 +0200

    fixed templates path

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="./templates")
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
